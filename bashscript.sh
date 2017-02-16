5d5b15a4a893c14910336f7f096e1907e7b5d642
pathanAbdullah <abdullapathan501@gmail.com>
2017-02-16 15:32:46 +0530

    commit to test

---
+++ bashscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "hello devops"
+
+VAR1="devops"
+FLOTE1=22.2
+NUM1=145
+
+echo $0
+echo $1
+echo $2
+
+
---
+++ varible.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+intA=20
+floteB=22.3
+stringC="first string"
+DIR_PATH="/home/devops/traning/bash"
+
+echo "values of integer A is  $intA"
+echo "values of flote B is  $floteB"
+echo "values of string C is  $stringC"
+echo "directory path is  $DIR_PATH"
+echo "content of TMP directory."
+ls $DIR_PATH
+
